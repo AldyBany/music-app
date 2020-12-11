43d64764afc167ddcd299191bc115129ea1c674c
Aldy Bany <64736339+AldyBany@users.noreply.github.com>
2020-12-12 01:27:56 +0200

    music app

---
+++ main.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    listFolded: true,
+    audio: [{
+        name: 'Invisible Beauty',
+        artist: 'Artist Name',
+        url: 'url.mp3',
+        cover: 'https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    }]
+});
