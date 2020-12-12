3204eaa406a07a66fb877d7f87fb7bd9016017df
Aldy Bany <64736339+AldyBany@users.noreply.github.com>
2020-12-12 13:06:27 +0200

    app music

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
+$(".album-poster").on('click', function (e) {
+    var dataSwitched = $(this).attr('data-switch')
+
+    ap.list.switch(dataSwitched)
+    ap.play()
+    $("#aplayer").addClass("showPlayer")
+    console.log(dataSwitched)
+
+})
+
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     listFolded: true,
-    audio: [{
-        name: 'Invisible Beauty',
-        artist: 'Artist Name',
-        url: 'url.mp3',
-        cover: 'https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-    }]
+    audio: [
+        {
+            name: 'Invisible Beauty',
+            artist: 'Artist Name',
+            url: 'songs/01 Track 1.wma',
+            cover: 'https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        },
+        {
+            name: 'Just Stay',
+            artist: 'Artist Name',
+            url: 'songs/01 Track 1.wma',
+            cover: 'https://images.pexels.com/photos/4651046/pexels-photo-4651046.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        },
+        {
+            name: 'Love first',
+            artist: 'Artist Name',
+            url: 'songs/01 Track 1.wma',
+            cover: 'https://images.pexels.com/photos/1771308/pexels-photo-1771308.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        },
+        {
+            name: 'Indivible',
+            artist: 'Artist Name',
+            url: 'songs/01 Track 1.wma',
+            cover: 'https://images.pexels.com/photos/2350325/pexels-photo-2350325.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        }
+    ]
 });
